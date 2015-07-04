e8fe3af3203c6df82ba389a37850662ffc70ff19
bostalowski <bostalowski@kaliop.com>
2015-07-04 16:08:46 +0200

    c   add arrow sprites C r

--- js/kuzzle-game.js
+++ js/kuzzle-game.js
@@ -2,13 +2,44 @@ KuzzleGame = typeof KuzzleGame === 'undefined' ? {} : KuzzleGame;
 
 KuzzleGame = {
 
+    game: null,
+    sprites: null,
 
-
-    preload: function() {
-        console.log('preload game');
+    instance: function() {
+        return this;
     },
 
+    /**
+     * Load your assets here. This is the first function launched
+     */
+    preload: function() {
+        this.game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
+    },
+
+    /**
+     * Initialize your variables here
+     */
     create: function() {
-        sprite.create(200, 360, 'mummy', 5);
+
+        var leftArrow = KuzzleGame.Arrow.Sprite;
+        leftArrow.create(this.game, 10, 10);
+        leftArrow.play();
+
+        var rightArrow = KuzzleGame.Arrow.Sprite;
+        rightArrow.create(this.game, 50, 10);
+        rightArrow.play();
+    },
+
+    /**
+     * Update your variables here. Typically, used for move your sprites (a loop is automaticaly launched by phaser)
+     */
+    update: function() {
+
+    },
+
+    /**
+     * Update your render here (Typically used for text)
+     */
+    render: function() {
     }
-}
+};
