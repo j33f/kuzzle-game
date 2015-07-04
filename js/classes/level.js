cbd159142516dfded997a489fdf15ef692659b39
bostalowski <bostalowski@kaliop.com>
2015-07-04 17:07:24 +0200

    modification de l'architecture , ajout d'une classe level pour gerer le niveau en cours

---
+++ js/classes/level.js
@@ -0,0 +1,47 @@
+KuzzleGame.Level = typeof KuzzleGame.Level === 'undefined' ? {} : KuzzleGame.Level;
+
+KuzzleGame.Level = {
+
+    debug: true,
+    arrowsMatrix: new Array(4),
+    ARROW_LEFT: 1,
+    ARROW_RIGHT: 2,
+    ARROW_UP: 3,
+    ARROW_DOWN: 4,
+    EMPTY_ARROW: 0,
+    elementToGeneratePerLevel: 200,
+    arrowsProbability: [0.6, 0.1, 0.1, 0.1, 0.1],
+
+    /**
+    * generate Level data (bi-dimentionnal array of arrows)
+    */
+    generateLevel: function() {
+
+        for(arrowMatrixIndex = 0 ; arrowMatrixIndex < this.arrowsMatrix.length ; arrowMatrixIndex++){
+
+            this.arrowsMatrix[arrowMatrixIndex] = new Array();
+
+            for(generatingIndex=0;generatingIndex<this.elementToGeneratePerLevel;generatingIndex++) {
+
+                this.arrowsMatrix[arrowMatrixIndex].push(this.generateRandomData(this));
+
+            }
+        }
+
+        if(this.debug){
+            console.log(this.arrowsMatrix);
+        }
+    },
+
+    generateRandomData: function() {
+        var r = Math.random();
+        var s = 0;
+
+        for(var i = 0; i < this.arrowsProbability.length; i++) {
+            s += this.arrowsProbability[i];
+            if(r <= s)
+                return i;
+        }
+    }
+
+};
--- js/kuzzle-game.js
+++ js/kuzzle-game.js
@@ -5,14 +5,6 @@ KuzzleGame = {
     game: null,
     sprites: null,
 
-    arrowsMatrix: new Array(4),
-    ARROW_LEFT: 1,
-    ARROW_RIGHT: 2,
-    ARROW_UP: 3,
-    ARROW_DOWN: 4,
-    EMPTY_ARROW: 0,
-    elementToGeneratePerLevel: 200,
-    arrowsProbability: [0.6, 0.1, 0.1, 0.1, 0.1],
 
     /**
      * Get KuzzleGame Instance
@@ -26,15 +18,16 @@ KuzzleGame = {
      * Load your assets here. This is the first function launched
      */
     preload: function() {
-        this.generateLevel();
-        this.game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
 
+        KuzzleGame.Level.generateLevel();
+        this.game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
     },
 
     /**
      * Initialize your variables here
      */
     create: function() {
+
         var leftArrow = KuzzleGame.Arrow.Sprite;
         leftArrow.create(this.game, 10, 10);
         leftArrow.play();
@@ -55,34 +48,6 @@ KuzzleGame = {
      * Update your render here (Typically used for text)
      */
     render: function() {
-    },
-
-    //Genere le tableau 2D qui contient l'ordre des fleche en fonction des probabilités renseignées.
-    generateLevel: function() {
-
-        for(arrowMatrixIndex = 0 ; arrowMatrixIndex < this.arrowsMatrix.length ; arrowMatrixIndex++){
-
-            this.arrowsMatrix[arrowMatrixIndex] = new Array();
-
-            for(generatingIndex=0;generatingIndex<this.elementToGeneratePerLevel;generatingIndex++) {
-
-                this.arrowsMatrix[arrowMatrixIndex].push(this.generateRandomData());
-
-            }
-        }
-
-        console.log(this.arrowsMatrix);
-    },
-
-    generateRandomData: function() {
-        var r = Math.random();
-        var s = 0;
-
-        for(var i = 0; i < this.arrowsProbability.length; i++) {
-            s += this.arrowsProbability[i];
-            if(r <= s)
-                return i;
-        }
     }
 
 };
