83caa3fa2698d1967f27eff7631d75d117d9ae7a
bostalowski <bostalowski@kaliop.com>
2015-07-05 17:50:32 +0200

    remove arrow only when out of game

--- js/classes/arrow.js
+++ js/classes/arrow.js
@@ -13,6 +13,7 @@ KuzzleGame.Arrow = {
             this.name = name;
             this.type = type;
             this.sprite = game.add.sprite(x, y, this.name);
+            this.sprite.visible = false;
             this.sprite.animations.add(KuzzleGame.Arrow.Animation.name);
             this.rectangle = new Phaser.Rectangle(x, y, this.sprite.width, this.sprite.height);
 
@@ -24,11 +25,16 @@ KuzzleGame.Arrow = {
         };
 
         this.update = function(game) {
+            //set visible juste when they get close to the main screen (earn frames ?)
+            if(this.sprite.y > - 500) {
+                this.sprite.visible = true;
+            }
+
             this.sprite.y += KuzzleGame.Arrow.Animation.speed * game.time.elapsed;
 
             this.rectangle.y = this.sprite.y;
 
-            if (this.sprite.y > 500)
+            if (this.sprite.y > game.height)
             {
                 this.remove();
                 return 0;
