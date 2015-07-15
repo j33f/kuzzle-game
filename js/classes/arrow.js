c865170b6c0b97e6ca67a77322f0eb1c6d0ee25e
bostalowski <bostalowski@kaliop.com>
2015-07-15 23:24:44 +0200

    changement des couleurs de fleches

--- js/classes/arrow.js
+++ js/classes/arrow.js
@@ -24,7 +24,24 @@ KuzzleGame.Arrow = {
         for(var i=0; i<KuzzleGame.Level.arrowsMatrix.length; i++) {
             var arrowType = KuzzleGame.Level.arrowsMatrix[i];
             if(arrowType != 0) {
-                var arrow = this.arrows.create(arrowType*100+10, 0 - (i*this.distanceBetweenArrows), 'arrows', 20+arrowType);
+
+                spriteOffset = 0;
+
+                if(arrowType == KuzzleGame.Level.ARROW_RIGHT){
+                    spriteOffset = 7;
+                }
+
+                if(arrowType == KuzzleGame.Level.ARROW_DOWN){
+                    spriteOffset = 18;
+                }
+
+                if(arrowType == KuzzleGame.Level.ARROW_UP){
+                    spriteOffset = 23;
+                }
+
+
+                var arrow = this.arrows.create(arrowType*100+10, 0 - (i*this.distanceBetweenArrows), 'arrows', spriteOffset);
+
                 arrow.y -= arrow.height/2;
                 arrow.name = 'arrow' + i;
                 arrow.type = arrowType;
